6048fd95d9dc4cb6b4e855cb1691e4f47e4c62f8
JonesTheG <4aflarsen@familie.tele.dk>
2022-01-10 07:26:41 +0100

    Added addition and custom scripts

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -31,14 +40,62 @@ const client = new discord_js_1.default.Client({
         discord_js_1.Intents.FLAGS.GUILD_MESSAGES
     ]
 });
+// when coding the bot, start with guild based for testing, then switch to global when it works
+// guild based slash command registered instantly
+// global registere to every guild or server the bot is in
 client.on('ready', () => {
+    var _a;
     console.log('The bot is ready');
+    const guildId = '927594699001331764';
+    const guild = client.guilds.cache.get(guildId);
+    let commands;
+    if (guild) {
+        commands = guild.commands;
+    }
+    else {
+        commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
+    }
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'ping',
+        description: 'Replies with pong.',
+    });
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'num1',
+                description: 'The first number.',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'The second number.',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    });
 });
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong'
+client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    const { commandName, options } = interaction;
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true,
         });
     }
-});
+    else if (commandName === 'add') {
+        const num1 = options.getNumber('num1');
+        const num2 = options.getNumber('num2');
+        interaction.reply({
+            content: `The sum is ${num1 + num2}`,
+            ephemeral: true
+        });
+    }
+}));
 client.login(process.env.TOKEN);
