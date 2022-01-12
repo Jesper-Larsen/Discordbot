dc9bf2290d1a1a6fe9266c259f545112ebb83c0b
JonesTheG <4aflarsen@familie.tele.dk>
2022-01-12 09:55:46 +0100

    added commands in seperate folder and simplified the deploy file

---
+++ commands/add.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('addition')
+		.setDescription('Addition')
+		.addNumberOption(option =>
+			option.setName('num1')
+				.setDescription('type a number')
+				.setRequired(true))
+		.addNumberOption(option =>
+			option.setName('num2')
+				.setDescription('type a number')
+				.setRequired(true)),
+	async execute(interaction) {
+		await interaction.reply({
+			content: `Your answer: 
+			${interaction.options.getNumber('num1') + interaction.options.getNumber('num2')}`, 
+			ephemeral: true });
+	},
+};
