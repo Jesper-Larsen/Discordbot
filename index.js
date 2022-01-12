4f06f2c8ba5f88e182533e6ec3f255069322c705
JonesTheG <4aflarsen@familie.tele.dk>
2022-01-12 08:26:37 +0100

    Started over

--- index.js
+++ index.js
@@ -1,101 +1,18 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = __importStar(require("discord.js"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const client = new discord_js_1.default.Client({
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+require('dotenv').config();
+
+// Create a new client instance
+const client = new Client({ 
     intents: [
-        discord_js_1.Intents.FLAGS.GUILDS,
-        discord_js_1.Intents.FLAGS.GUILD_MESSAGES
-    ]
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
 });
-// when coding the bot, start with guild based for testing, then switch to global when it works
-// guild based slash command registered instantly
-// global registere to every guild or server the bot is in
-client.on('ready', () => {
-    var _a;
-    console.log('The bot is ready');
-    const guildId = '927594699001331764';
-    const guild = client.guilds.cache.get(guildId);
-    let commands;
-    if (guild) {
-        commands = guild.commands;
-    }
-    else {
-        commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
-    }
-    commands === null || commands === void 0 ? void 0 : commands.create({
-        name: 'ping',
-        description: 'Replies with pong.',
-    });
-    commands === null || commands === void 0 ? void 0 : commands.create({
-        name: 'add',
-        description: 'Adds two numbers.',
-        options: [
-            {
-                name: 'num1',
-                description: 'The first number.',
-                required: true,
-                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
-            },
-            {
-                name: 'num2',
-                description: 'The second number.',
-                required: true,
-                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
-            }
-        ]
-    });
-});
-client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
-    if (!interaction.isCommand()) {
-        return;
-    }
-    const { commandName, options } = interaction;
-    if (commandName === 'ping') {
-        interaction.reply({
-            content: 'pong',
-            ephemeral: true,
-        });
-    }
-    else if (commandName === 'add') {
-        const num1 = options.getNumber('num1');
-        const num2 = options.getNumber('num2');
-        interaction.reply({
-            content: `The sum is ${num1 + num2}`,
-            ephemeral: true
-        });
-    }
-}));
+
+// Login to Discord with your client's token
 client.login(process.env.TOKEN);
